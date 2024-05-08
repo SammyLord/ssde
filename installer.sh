58cc7e4894f56fd397b89929ed12bd5296196717
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-05-08 17:00:12 +0000

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,5 +1,8 @@
-sudo apt install openbox xfce4-panel lxappearance nitrogen xterm gthumb thunar obconf
+sudo apt install openbox xfce4-panel lxappearance nitrogen lxterminal gthumb thunar obconf xfce4-screenshooter arandr
 sudo cp -r ./autostart /etc/xdg/openbox/autostart
 ./fetchkisser/fetchkisser.sh
 chmod +x ././fetchkisser/fetchkisser.sh
 sudo cp -r ./fetchkisser/fetchkisser.sh /usr/bin/fetchkisser
+git clone https://github.com/Sneed-Group/sparksammys-dotfiles
+cp -r "sparksammys-dotfiles/.config" ~
+cp -r "sparksammys-dotfiles/.local" ~
