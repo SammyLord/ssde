00d8b8b7fa6b237462187c5a0a054dc933f57ad4
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-08-23 17:08:54 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-sudo apt install openbox xfce4-panel lxappearance nitrogen lxterminal gthumb thunar obconf xfce4-screenshooter arandr
+sudo apt install openbox xfce4-panel lxappearance nitrogen lxterminal gthumb thunar obconf xfce4-screenshooter arandr blueman
 sudo cp -r ./autostart /etc/xdg/openbox/autostart
 ./fetchkisser/fetchkisser.sh
 chmod +x ././fetchkisser/fetchkisser.sh
