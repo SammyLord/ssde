b8aed1f74b49d7ddb78c7dba6262859639ec9dc6
The Ghost of FOSS' Future <163201376+sammylord-holder@users.noreply.github.com>
2025-05-20 19:27:05 +0000

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
-sudo apt install xinit lolcat openbox xfce4-panel lxappearance nitrogen lxterminal gthumb thunar obconf xfce4-screenshooter arandr blueman pcmanfm pipewire-alsa pipewire-audio pavucontrol gthumb sudo network-manager-gnome network-manager
+sudo apt install openbox xfce4-panel lxappearance nitrogen lxterminal gthumb thunar obconf xfce4-screenshooter arandr blueman pcmanfm pipewire-alsa pipewire-audio pavucontrol gthumb sudo network-manager-gnome network-manager
+sudo apt install network-manager-vpnc network-manager-openvpn network-manager-gnome
 sudo cp -r ./autostart /etc/xdg/openbox/autostart
-git clone https://github.com/SammyLord/fetchkisser
+git clone https://github.com/Sneed-Group/fetchkisser
 chmod +x ././fetchkisser/fetchkisser.sh
 sudo cp -r ./fetchkisser/fetchkisser.sh /usr/bin/fetchkisser
-git clone https://github.com/SammyLord/sparksammys-dotfiles
 cp -r "sparksammys-dotfiles/.config" ~
 cp -r "sparksammys-dotfiles/.local" ~
 wget https://raw.githubusercontent.com/the-spellman/installer-v3/main/pack.sh
@@ -14,3 +14,4 @@ echo "Also optionally run remove-cinnamon.sh if youre on Linux Mint 22"
 
 echo "fetchkisser -bk | lolcat" > ~/.bashrc
 echo "cat /dev/null > ~/.bash_history && history -c" ~/.bashrc
+sudo cp 01-network-manager-all.yaml /etc/netplan
